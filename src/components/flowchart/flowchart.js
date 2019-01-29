637640c5fb1339e8193ccf8319705aaf74d23c27
leechikit <leechikit0823@163.com>
2019-01-29 15:12:52 +0800

    update

--- src/components/flowchart/flowchart.js
+++ src/components/flowchart/flowchart.js
@@ -8,24 +8,22 @@ G6.registerNode('customNode', {
     [0,0.5]
   ],
   draw(item){
-    const defaultWidth = 122
-    const defaultHeight = 42
     const group = item.getGraphicGroup()
     const model = item.getModel()
     group.addShape('rect', {
       attrs: {
         x: 0,
         y: 0,
-        width: defaultWidth,
-        height: defaultHeight,
+        width: model.width,
+        height: model.height,
         stroke: '#37ABFD',
         fill: model.isRun ? '#37ABFD' : ''
       }
     })
     group.addShape('text', {
       attrs: {
-        x: defaultWidth / 2,
-        y: defaultHeight / 2,
+        x: model.width / 2,
+        y: model.height / 2,
         fill: model.isRun ? '#fff' : '#000',
         text: model.text,
         textAlign: 'center',
--- src/components/flowchart/flowchart.vue
+++ src/components/flowchart/flowchart.vue
@@ -5,6 +5,8 @@
 import G6 from '@antv/g6';
 import './flowchart.js'
 import API from '@/api/api.json'
+const winW = document.body.offsetWidth
+
 export default {
   name: 'flowchart',
   data () {
@@ -16,7 +18,8 @@ export default {
   mounted () {
     const Activities = API.ReturnData.datas.WorkflowTemplate.Data.Activities
     const Rules = API.ReturnData.datas.WorkflowTemplate.Data.Rules
-    this.setNodes(Activities)
+    const { graphWidth, graphHeight, minX, minY } = this.getGraphSize(Activities)
+    this.setNodes(Activities, minX, minY)
     this.setEdges(Rules)
     const data = {
       nodes: JSON.parse(JSON.stringify(this.nodes)),
@@ -25,19 +28,21 @@ export default {
     const graph = new G6.Graph({
       container: 'mountNode',
       fitview: 'autoZoom', // 为了防止布局超出屏幕，使用 fitview: 'autoZoom' 进行自动放缩
-      width: 800,
-      height: 600
+      width: graphWidth,
+      height: graphHeight
     });
     graph.read(data);
   },
   methods: {
-    setNodes (list) {
+    setNodes (list, minX, minY) {
       list.forEach(item => {
         this.nodes.push({
           shape: 'customNode',
           id: item.ActivityCode,
-          x: item.X,
-          y: item.Y,
+          x: item.X - minX,
+          y: item.Y - minY,
+          width: item.Width,
+          height: item.Height,
           text: item.DisplayName,
           isRun: item.isRun
         })
@@ -53,9 +58,35 @@ export default {
           endArrow: true
         })
       })
+    },
+    getGraphSize (list) {
+      let maxX = 0
+      let maxY = 0
+      let minX = 0
+      let minY = 0
+      list.forEach((item, index) => {
+        if (index === 0) {
+          minX = item.X
+          minY = item.Y
+        } else {
+          minX = item.X < minX ? item.X : minX
+          minY = item.Y < minY ? item.Y : minY
+        }
+        maxX = (item.X + item.Width) > maxX ? item.X + item.Width : maxX
+        maxY = (item.Y + item.Height) > maxY ? item.Y + item.Height : maxY
+      })
+      return {
+        graphWidth: maxX - minX,
+        graphHeight: maxY - minY,
+        minX,
+        minY
+      }
     }
   }
 }
 </script>
-<style lang="scss" scoped>
+<style lang="css" scoped>
+#mountNode {
+  overflow: auto;
+}
 </style>
